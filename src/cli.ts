2c9c15cc9a4f4c727c24973c1f3e630b90a03799
Naman Sood <54349762+namansoood@users.noreply.github.com>
2025-03-09 11:54:48 +0530

    login & publish prototype

--- src/cli.ts
+++ src/cli.ts
@@ -5,12 +5,28 @@ import path from "path";
 import { build } from "esbuild";
 import { parse as reactDocParse } from "react-docgen";
 import fg from "fast-glob";
+import chalk from "chalk";
+import { execSync } from "child_process";
 
 const NAVSDK_CONFIG_FILE_NAME = "navsdk.config.json";
 
 type Config = {
 	paths: string[];
 	outputDir: string;
+}
+
+function getVersion() {
+	try {
+		const output = execSync(`npm list -g @makerinc/nav-sdk --depth=0 --json`, { encoding: "utf-8" });
+		const parsed = JSON.parse(output);
+		return parsed.dependencies?.["@makerinc/nav-sdk"]?.version || chalk.red("No version detected");
+	} catch (error) {
+		return chalk.red("No version detected");
+	}
+}
+
+function helloWorld() {
+	console.log(chalk.bold(chalk.cyan("Nav SDK")), chalk.cyan(getVersion()));
 }
 
 function makeNewConfig(): Config {
@@ -36,7 +52,7 @@ async function getConfig(): Promise<Config> {
 	return config;
 }
 
-async function buildComponent(componentPath: string, config: Config) {
+async function buildComponent(componentPath: string, config: Config): Promise<boolean> {
 	const componentName = path.basename(componentPath, path.extname(componentPath));
 	const componentOutFile = path.join(config.outputDir, `${componentName}.js`);
 	const reactdocOutFile = path.join(config.outputDir, `${componentName}.doc.json`);
@@ -59,46 +75,107 @@ async function buildComponent(componentPath: string, config: Config) {
 		const metadata = metadataArray[0];
 
 		if (!metadata || !metadata.props) {
-			console.log("\x1b[31m", "\u2717\ufe0e No props found for the component " + componentPath);
-			return;
+			console.log(chalk.red("\u2717\ufe0e No props found for the component " + componentPath));
+			return false;
 		}
 
 		fs.writeFileSync(reactdocOutFile, JSON.stringify(metadata, null, 2));
 
-		console.log("\x1b[32m", `\u2714\ufe0e Built ${componentOutFile}`);
+		console.log(chalk.green("\u2714\ufe0e Built " + componentOutFile));
+		return true;
 	} catch (error) {
-		console.log("\x1b[31m", `\u2717\ufe0e Failed to build ${componentPath} - ${error}`);
+		console.log(chalk.red(`\u2717\ufe0e Failed to build ${componentPath} - ${error}`));
+		return false;
 	}
 }
 
-async function buildComponents() {
+const sleep = (delay: number) => {
+	return new Promise(resolve => setTimeout(resolve, delay));
+}
+
+async function login() {
+	console.log("Logging in...")
+	await sleep(1000)
+	console.log(chalk.green("Login successful."));
+}
+
+async function buildComponents(): Promise<boolean> {
 	const config = await getConfig();
 	if (!config) {
-		console.log("\x1b[31m", "\u2717\ufe0e navsdk.config.json not found. Please create a config file to build your components.");
-		return;
+		console.log(chalk.red("\u2717\ufe0e navsdk.config.json not found. Please create a config file to build your components."));
+		return false;
 	}
 
 	let files: string[] = await fg(config.paths);
 
-	for (const file of files) {
-		await buildComponent(file, config);
+	console.log("Building components...");
+
+	let buildSuccessCount = 0;
+	let buildFailureCount = 0;
+
+	await Promise.all(files.map(async (file) => {
+		const success = await buildComponent(file, config);
+		if (success) {
+			buildSuccessCount++;
+		} else {
+			buildFailureCount++;
+		}
+	}));
+
+	if (buildFailureCount > 0) {
+		console.log(chalk.bold(chalk.red(`\u2717\ufe0e ${buildFailureCount} component${buildFailureCount > 1 ? "s" : ""} failed to build.`)));
+		return false;
+	} else if (buildSuccessCount > 0) {
+		console.log(chalk.bold(chalk.green(`\u2714\ufe0e Build successful.`)));
+		return true;
+	} else {
+		console.log(chalk.bold(chalk.red("\u2717\ufe0e No components found.")));
+		return false;
 	}
 }
 
+async function publish() {
+	console.log("Publishing...")
+	await sleep(1000)
+	console.log(chalk.green("Publish successful."))
+}
+
 const args = process.argv.slice(2);
-const command = args[0];
+const command = args[0] || "";
 const help = args[1] == "-h" || args[1] == "--help";
 
 (async () => {
+	helloWorld();
 	switch (command) {
+		case "":
+			break;
+		case "help":
+			console.log("Available commands are `login`, `build`, `publish`.");
+			break;
+		case "login":
+			if (help) {
+				console.log("Login to Nav editor.\n\nUsage: nav-sdk login");
+			} else {
+				await login();
+			}
+			break;
 		case "build":
 			if (help) {
-				console.log("Build a custom component to Nav editor.\n\nUsage: nav-sdk build");
+				console.log("Build your custom components. \n\nUsage: nav-sdk build");
 			} else {
 				await buildComponents();
 			}
 			break;
+		case "publish":
+			if (help) {
+				console.log("Publishes your built custom component. Requires you to be logged into the Nav editor.\n\nUsage: nav-sdk publish");
+			} else {
+				await login();
+				await buildComponents();
+				await publish();
+			}
+			break;
 		default:
-			console.log("Unknown command. Supported commands is `build`.");
+			console.log(chalk.red("Unknown command \"" + command + "\". Supported commands are `login`, `build`, `publish`."));
 	}
 })();
