0790fadb7cca3e81e6e3e4b1f6f7d41488d49bd0
Naman Sood <54349762+namansoood@users.noreply.github.com>
2025-05-05 14:49:46 +0530

    Rename NavProductBuyButton -> NavBuyButton and remove duplicate types in SharedComponents

--- src/components/SharedComponents.tsx
+++ src/components/SharedComponents.tsx
@@ -1,7 +1,7 @@
 import React from '../react';
 import { Props as ImageProps } from './shared/NavImage';
 import { Props as VideoProps } from './shared/NavVideo';
-import { Props as BuyButtonProps } from './shared/NavProductBuyButton';
+import { Props as BuyButtonProps } from './shared/NavBuyButton';
 import { Props as ProductFormProps } from './shared/NavProductForm';
 import { Props as ProductVariantPickerProps } from './shared/NavProductVariantPicker';
 import { Props as ProductQuantityPicker } from './shared/NavProductQuantityPicker';
@@ -31,22 +31,13 @@ window.__MAKER_NAV_SHARED_COMPONENTS_CONTEXT__ = window.__MAKER_NAV_SHARED_COMPO
 
 const Context = window.__MAKER_NAV_SHARED_COMPONENTS_CONTEXT__;
 
-type ProviderProps = React.PropsWithChildren<{
-	renderImage: ((props: ImageProps) => React.JSX.Element) | undefined;
-	renderVideo: ((props: VideoProps) => React.JSX.Element) | undefined;
-	renderProductLink: ((props: ProductLinkProps) => React.JSX.Element) | undefined;
-	renderCategoryLink: ((props: CategoryLinkProps) => React.JSX.Element) | undefined;
-	renderBuyButton: ((props: BuyButtonProps) => React.JSX.Element) | undefined;
-	renderProductForm: ((props: ProductFormProps) => React.JSX.Element) | undefined;
-	renderProductVariantPicker: ((props: ProductVariantPickerProps) => React.JSX.Element) | undefined;
-	renderProductQuantityPicker: ((props: ProductQuantityPicker) => React.JSX.Element) | undefined;
-}>;
+type ProviderProps = React.PropsWithChildren<ContextType>;
 
 export const useContext = () => React.useContext(Context);
 
-export function Provider({ renderImage, renderVideo, renderProductLink, renderCategoryLink, renderBuyButton, renderProductForm, renderProductVariantPicker, renderProductQuantityPicker, children }: ProviderProps) {
+export function Provider({ children, ...ctxValue }: ProviderProps) {
 	return (
-		<Context.Provider value={{ renderImage, renderVideo, renderProductLink, renderCategoryLink, renderBuyButton, renderProductForm, renderProductVariantPicker, renderProductQuantityPicker }}>
+		<Context.Provider value={ctxValue}>
 			{children}
 		</Context.Provider>
 	);
